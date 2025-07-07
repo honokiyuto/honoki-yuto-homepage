9cbd02aa12207335eb861cffcf96ffab16d1948e
honokiyuto <honokiyuto@team-lab.com>
2025-07-07 13:07:27 +0900

    feat: セクションコンポーネントを作成

--- src/components/layout/TextTyping/index.tsx
+++ src/components/layout/TextTyping/index.tsx
@@ -1,22 +1,52 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 type Props = {
-  text: string;
+  children: string;
+  isGradient?: boolean;
 };
 
-export const TextTyping = ({ text }: Props) => {
+export const TextTyping = ({ children, isGradient = false }: Props) => {
   const [displayedText, setDisplayedText] = useState('');
+  const elementRef = useRef<HTMLHeadingElement>(null);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setDisplayedText(text.slice(0, displayedText.length + 1));
-    }, 100);
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            let currentLength = 0;
+            const interval = setInterval(() => {
+              if (currentLength <= children.length) {
+                setDisplayedText(children.slice(0, currentLength));
+                currentLength++;
+              } else {
+                clearInterval(interval);
+              }
+            }, 100);
 
-    return () => clearInterval(interval);
-  }, [text, displayedText]);
+            observer.disconnect();
+          }
+        });
+      },
+      { threshold: 0.5 }
+    );
+
+    if (elementRef.current) {
+      observer.observe(elementRef.current);
+    }
+
+    return () => observer.disconnect();
+  }, [children]);
 
   return (
-    <h2 className="text-2xl font-bold font-oswald after:content-['|'] after:animate-typing after:p-2">
+    <h2
+      ref={elementRef}
+      className={`text-4xl/relaxed tracking-wider uppercase after:content-['|'] after:animate-typing after:p-2 ${
+        isGradient
+          ? 'bg-gradient-to-r from-indigo-600 via-fuchsia-400 to-amber-200 bg-clip-text text-transparent'
+          : ''
+      }`}
+    >
       {displayedText}
     </h2>
   );
--- src/components/sections/index.tsx
+++ src/components/sections/index.tsx
@@ -3,32 +3,77 @@ import { TextTyping } from '../layout/TextTyping';
 
 export const sections: Section[] = [
   {
-    anchorLinkName: 'section-a',
+    anchorLinkName: 'top',
     left: (
-      <div className="bg-red-500 h-full flex items-center justify-center">
-        <TextTyping text="Section A(left)" />
+      <div className="h-full flex items-center justify-center">
+        <div className="p-10 max-w-3/4">
+          <TextTyping isGradient>
+            Welcome to the Cafe's Entrance 2023
+          </TextTyping>
+        </div>
       </div>
     ),
-    right: <div className="bg-blue-500 h-full flex items-center justify-center">Section A(right)</div>,
+    right: (
+      <div className="bg-linear-to-br from-indigo-600 via-fuchsia-400 to-amber-200 h-full flex items-center justify-center"></div>
+    ),
   },
   {
-    anchorLinkName: 'section-b', 
-    left: <div className="bg-green-500 h-full flex items-center justify-center">Section B(left)</div>,
-    right: <div className="bg-yellow-500 h-full flex items-center justify-center">Section B(right)</div>,
+    anchorLinkName: 'about',
+    left: (
+      <div className="h-full flex flex-col items-center justify-center gap-5 p-10">
+        <TextTyping>about</TextTyping>
+        <p className="text-center">
+          「Cafe」と申します。このホームページは私の趣味を発信する場として、今後の活動の出発点としたいと考えています。「エンジニアとしての私」、「絵師としての私」、「公認心理師としての私」の3つを通じて、自分を表現していけたらと思っています。
+        </p>
+      </div>
+    ),
+    right: (
+      <div className="h-full flex items-center justify-center">
+        <img
+          src="/img/sign_img.png"
+          alt="about"
+          className="h-full object-contain"
+        />
+      </div>
+    ),
   },
   {
     anchorLinkName: 'section-c',
-    left: <div className="bg-purple-500 h-full flex items-center justify-center">Section C(left)</div>,
-    right: <div className="bg-pink-500 h-full flex items-center justify-center">Section C(right)</div>,
+    left: (
+      <div className="h-full flex items-center justify-center">
+        <TextTyping>Section C</TextTyping>
+      </div>
+    ),
+    right: (
+      <div className="h-full flex items-center justify-center">
+        <TextTyping>Section C(right)</TextTyping>
+      </div>
+    ),
   },
   {
     anchorLinkName: 'section-d',
-    left: <div className="bg-indigo-500 h-full flex items-center justify-center">Section D(left)</div>,
-    right: <div className="bg-cyan-500 h-full flex items-center justify-center">Section D(right)</div>,
+    left: (
+      <div className="h-full flex items-center justify-center">
+        <TextTyping>Section D</TextTyping>
+      </div>
+    ),
+    right: (
+      <div className="bg-cyan-500 h-full flex items-center justify-center">
+        <TextTyping>Section D(right)</TextTyping>
+      </div>
+    ),
   },
   {
     anchorLinkName: 'section-e',
-    left: <div className="bg-orange-500 h-full flex items-center justify-center">Section E(left)</div>,
-    right: <div className="bg-teal-500 h-full flex items-center justify-center">Section E(right)</div>,
+    left: (
+      <div className="bg-orange-500 h-full flex items-center justify-center">
+        <TextTyping>Section E</TextTyping>
+      </div>
+    ),
+    right: (
+      <div className="bg-teal-500 h-full flex items-center justify-center">
+        <TextTyping>Section E(right)</TextTyping>
+      </div>
+    ),
   },
 ];
