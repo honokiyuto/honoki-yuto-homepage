3dfa54852c432218ad1b570b07c9d6391472e131
honokiyuto <honokiyuto@team-lab.com>
2025-07-09 10:51:48 +0900

    fix: ghaの設定を最新にする

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://honokiyuto.github.io',
+  base: 'honoki-yuto-homepage',
   devToolbar: {
     enabled: false,
   },
