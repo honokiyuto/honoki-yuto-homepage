9807a4e6af8ba47685039c14c1ec7e051463a2fc
honokiyuto <honokiyuto@team-lab.com>
2025-07-09 18:46:24 +0900

    fix: baseUrlを削除する

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://honokiyuto.github.io',
-  base: 'honoki-yuto-homepage',
+  base: '',
   devToolbar: {
     enabled: false,
   },
